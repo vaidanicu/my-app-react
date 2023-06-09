9025db403c07e6c520669dc99b8c9cab2cee135d
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-06-09 22:58:34 +0300

    MappingData to Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import contacts from "../contacts";
 import Card from "./Card";
-import Avatar from "./avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 export default function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar img="https://avatars.githubusercontent.com/u/64326133?v=4" />
-      <Card
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -25,7 +38,7 @@ export default function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
